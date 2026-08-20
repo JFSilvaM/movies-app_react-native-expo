58f62d1c0c21b2a5f16535d0cd8a4bd097da5b57
JFSilvaM <joaofrsima2000@gmail.com>
2026-08-20 10:18:09 +0200

    feat: add movies api config

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,11 +1,14 @@
+import { nowPlayingAction } from "@/core/actions/movies/now-playing.action";
 import { Text, View } from "react-native";
 
-import "../../global.css";
+import "../global.css";
 
 const RootLayout = () => {
+  nowPlayingAction();
+
   return (
     <View>
-      <Text>RootLayout</Text>
+      <Text className="text-3xl text-blue-500">RootLayout</Text>
     </View>
   );
 };
